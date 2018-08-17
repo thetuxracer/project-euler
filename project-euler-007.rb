1134f01cd8207593a8d50e8e2009662c9fedf676
Kaustubh Padegaonkar <kpadegao@redhat.com>
2018-08-17 10:29:23 +0530

    PE-007 - Made readable, worsened performance

--- project-euler-007.rb
+++ project-euler-007.rb
@@ -1,26 +1,41 @@
 require 'benchmark'
-puts Benchmark.measure{
-  INFINITY = 110000
+
+INFINITY = 110000
+
+def go_prime
+  
   nth = 1
-  primes = []
+  primes = [2]
   (1..INFINITY).each do |i|
     factor = 0
     
-    next if i%2 == 0
+    next if is_even i
 
     (3..i).each do |j|
-      if (i%j == 0)
-        factor += 1
-        break if factor > 1
-      end
+      
+      next if is_even j
+      
+      factor += 1 if can_factorize i, j
       break if factor > 1
     end
-    if (factor == 1)
-      primes << i 
-      nth += 1
-    end
+
+    next if factor > 1
+    primes << i 
+    nth += 1
 
     break if nth == 10001
   end
+  puts "Found #{primes.size} primes."
   puts primes.last
-}
+end
+
+def is_even number
+  number%2 == 0
+end
+
+def can_factorize big_num, small_num
+  big_num%small_num == 0
+end
+
+puts Benchmark.measure{go_prime}
+
